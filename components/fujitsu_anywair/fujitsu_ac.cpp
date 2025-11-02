a12b13d99348a2a3c37c201e50f113139dce2dc0
memphisraynz <daniel.rayner@gmail.com>
2025-11-02 10:17:48 +1000

    updates

--- components/fujitsu_anywair/fujitsu_ac.cpp
+++ components/fujitsu_anywair/fujitsu_ac.cpp
@@ -1,32 +1,33 @@
-#include "fujitsu_anywair.h"
+#include "fujitsu_ac.h"
 
 namespace esphome {
 namespace fujitsu_anywair {
-  // This will be called by ESPHome once to initialize the component.
+
+void FujitsuAnywAIRClimate::setup() {
   ESP_LOGCONFIG(TAG, "Setting up Fujitsu AC Climate component");
-  // Here you could initiate UART communication or anything specific.
 }
 
 void FujitsuAnywAIRClimate::control(const climate::ClimateCall &call) {
-  // This is called when there is a request to change HVAC mode, temperature, etc.
-  // For now, just log what was requested.
   ESP_LOGD(TAG, "Received control command");
   if (call.get_target_temperature()) {
     ESP_LOGD(TAG, "Target temperature: %.1f", *call.get_target_temperature());
   }
-  // Add HVAC control implementation here later.
+  // Implement control logic here
 }
 
 climate::ClimateTraits FujitsuAnywAIRClimate::traits() {
   auto traits = climate::ClimateTraits();
-
   traits.set_supports_current_temperature(true);
   traits.set_supports_target_temperature(true);
-  traits.set_supports_operation_mode(climate::CLIMATE_OPERATION_MODE_OFF | climate::CLIMATE_OPERATION_MODE_HEAT);
+  traits.set_supports_operation_mode(
+      climate::CLIMATE_OPERATION_MODE_OFF | climate::CLIMATE_OPERATION_MODE_HEAT |
+      (supports_cool_ ? climate::CLIMATE_OPERATION_MODE_COOL : 0));
   traits.min_temperature = 16.0f;
   traits.max_temperature = 30.0f;
   traits.precision = 0.5f;
 
   return traits;
+}
+
 }  // namespace fujitsu_anywair
 }  // namespace esphome
--- components/fujitsu_anywair/fujitsu_ac.h
+++ components/fujitsu_anywair/fujitsu_ac.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "esphome.h"
+#include "esphome/components/climate/climate.h"
+#include "esphome/components/uart/uart.h"
+
 namespace esphome {
 namespace fujitsu_anywair {
 
@@ -10,9 +14,13 @@ class FujitsuAnywAIRClimate : public climate::Climate, public Component {
   climate::ClimateTraits traits() override;
 
   void set_uart(uart::UARTComponent *uart) { this->uart_ = uart; }
+  void set_supports_cool(bool supports_cool) { supports_cool_ = supports_cool; }
+  void set_supports_heat(bool supports_heat) { supports_heat_ = supports_heat; }
 
  protected:
-  uart::UARTComponent *uart_;
+  uart::UARTComponent *uart_{nullptr};
+  bool supports_cool_{true};
+  bool supports_heat_{true};
 };
 
 }  // namespace fujitsu_anywair
