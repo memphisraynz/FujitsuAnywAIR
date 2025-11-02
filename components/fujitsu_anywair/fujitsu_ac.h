a1d9ceeb73bb037074550f8ed4523acdb4852bcb
memphisraynz <daniel.rayner@gmail.com>
2025-11-02 10:20:10 +1000

    uart potential fix

--- components/fujitsu_anywair/fujitsu_ac.h
+++ components/fujitsu_anywair/fujitsu_ac.h
@@ -13,7 +13,9 @@ class FujitsuAnywAIRClimate : public climate::Climate, public Component {
   void control(const climate::ClimateCall &call) override;
   climate::ClimateTraits traits() override;
 
-  void set_uart(uart::UARTComponent *uart) { this->uart_ = uart; }
+  void set_uart_parent(uart::UARTComponent *parent) {
+    this->set_uart(parent);
+  }
   void set_supports_cool(bool supports_cool) { supports_cool_ = supports_cool; }
   void set_supports_heat(bool supports_heat) { supports_heat_ = supports_heat; }
 
