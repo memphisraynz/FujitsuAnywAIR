7ffbf4a96bf9a5a8ab8bc20cd21c020fafdd8352
memphisraynz <daniel.rayner@gmail.com>
2025-11-02 10:11:25 +1000

    updating base files

--- components/fujitsu_anywair/fujitsu_ac.h
+++ components/fujitsu_anywair/fujitsu_ac.h
@@ -4,7 +4,15 @@ namespace esphome {
 namespace fujitsu_anywair {
 
 class FujitsuAnywAIRClimate : public climate::Climate, public Component {
+ public:
+  void setup() override;
+  void control(const climate::ClimateCall &call) override;
+  climate::ClimateTraits traits() override;
 
+  void set_uart(uart::UARTComponent *uart) { this->uart_ = uart; }
+
+ protected:
+  uart::UARTComponent *uart_;
 };
 
 }  // namespace fujitsu_anywair
