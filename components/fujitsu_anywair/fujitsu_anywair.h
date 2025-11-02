4db511b0bfcbbc24e3f67639d85e987ffc07fdd7
memphisraynz <daniel.rayner@gmail.com>
2025-11-02 13:49:55 +1000

    class update

--- components/fujitsu_anywair/fujitsu_anywair.h
+++ components/fujitsu_anywair/fujitsu_anywair.h
@@ -13,10 +13,12 @@ class FujitsuAnywAIRClimate : public climate::Climate, public uart::UARTDevice, 
   void control(const climate::ClimateCall &call) override;
   climate::ClimateTraits traits() override;
 
+  void set_uart(uart::UARTComponent *uart) { this->uart_ = uart; }
   void set_supports_cool(bool supports_cool) { supports_cool_ = supports_cool; }
   void set_supports_heat(bool supports_heat) { supports_heat_ = supports_heat; }
 
  protected:
+  uart::UARTComponent *uart_{nullptr};
   bool supports_cool_{true};
   bool supports_heat_{true};
 };
